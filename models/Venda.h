0a663f185afa6c041852f4413d27858bc171a5d2
Jorge Emanoel de Oliveira Pereira <eu.jorge.emanoell@gmail.com>
2020-01-31 02:22:29 -0300

    corrige cadastro da venda

--- models/Venda.h
+++ models/Venda.h
@@ -3,18 +3,21 @@ int venda_autoincremento = 0;
 typedef struct {
     int codigo;
     int quantidade;
-    char data[11];
+    char data[10];
     float preco;
     int funcionario_codigo;
     int produto_codigo;
+    char motivo[200];
 } Venda;
 
 
-void cadastrarVenda();
 void salvarVenda(Venda venda);
 void listarVendas();
 void copiarVendas();
 int existeVenda(int codigo);
+int contarVendas();
+int contarVendasFuncionario(int codigo);
+int contarVendasProduto(int codigo);
 Venda lerVenda();
 Venda buscarVenda();
 Venda removerVenda();
@@ -29,6 +32,38 @@ int contarVendas() {
     }
     while (fread(&tmpVenda, sizeof(Venda), 1, f)) {
         qtd++;
+    }
+    fclose(f);
+    return qtd;
+}
+
+int contarVendasFuncionario(int codigo) {
+    FILE *f;
+    Venda tmpVenda;
+    f = fopen("database/vendas.bin", "rb");
+    int qtd = 0;
+    if (!f) {
+        return 0;
+    }
+    while (fread(&tmpVenda, sizeof(Venda), 1, f)) {
+        if (tmpVenda.funcionario_codigo == codigo)
+            qtd++;
+    }
+    fclose(f);
+    return qtd;
+}
+
+int contarVendasProduto(int codigo) {
+    FILE *f;
+    Venda tmpVenda;
+    f = fopen("database/vendas.bin", "rb");
+    int qtd = 0;
+    if (!f) {
+        return 0;
+    }
+    while (fread(&tmpVenda, sizeof(Venda), 1, f)) {
+        if (tmpVenda.produto_codigo == codigo)
+            qtd++;
     }
     fclose(f);
     return qtd;
@@ -64,16 +99,6 @@ void copiarVendas() {
     fclose(tf);
 }
 
-void cadastrarVenda() {
-    printf("Cadastro de venda:\n");
-    getchar();
-
-    Venda venda = lerVenda();
-    salvarVenda(venda);
-    printf("Venda salvo com sucesso.");
-
-}
-
 void salvarVenda(Venda venda) {
     FILE *f, *tf;
     Venda tmpVenda;
@@ -101,19 +126,35 @@ void salvarVenda(Venda venda) {
 }
 
 void listarVendas() {
-    printf("=================== Lista de vendas cadastrados ==================\n\n");
     FILE *f;
     f = fopen("database/vendas.bin", "rb");
     char tmp;
     Venda tmpVenda;
     while (fread(&tmpVenda, sizeof(Venda), 1, f)) {
+        Funcionario funcionario = buscarFuncionario(tmpVenda.funcionario_codigo);
         printf("Código: %d\n", tmpVenda.codigo);
         printf("Data: %s\n", tmpVenda.data);
         printf("Preço: %.2f\n", tmpVenda.preco);
         printf("Quantidade: %d\n", tmpVenda.quantidade);
-        printf("Valor total: %.2f\n", tmpVenda.quantidade * tmpVenda.quantidade);
+        printf("Valor total: %.2f\n", tmpVenda.quantidade * tmpVenda.preco);
+        printf("Funcionário: %s (ID: %d)\n\n", funcionario.nome, tmpVenda.funcionario_codigo);
     }
-    printf("====================================================================\n\n");
+    fclose(f);
+}
+
+void salvarVendaNegativa(Venda venda) {
+    FILE *f;
+    strcpy(venda.motivo, "Sem estoque");
+    f = fopen("database/vendas-erradas.bin", "ab");
+    fwrite(&venda, sizeof(Venda), 1, f);
+    fclose(f);
+}
+
+void salvarVendaSemEstoque(Venda venda) {
+    FILE *f;
+    strcpy(venda.motivo, "Quantidade mínima atingida");
+    f = fopen("database/vendas-erradas.bin", "ab");
+    fwrite(&venda, sizeof(Venda), 1, f);
     fclose(f);
 }
 
@@ -121,17 +162,79 @@ Venda lerVenda() {
     Venda novoVenda;
     novoVenda.codigo = 0;
 
-    printf("Data: ");
+    printf("Data (dd/mm/aaaa): ");
     stread(novoVenda.data, 11);
+    while (!datevalid(novoVenda.data)) {
+        printf("Data inválida (%s). Utilize o formato: dd/mm/aaaa\n", novoVenda.data);
+        printf("Data da venda: ");
+        stread(novoVenda.data, 11);
+    }
 
+    int i = 0;
     printf("Código do produto: ");
-    scanf("%d", &novoVenda.produto_codigo);
+    scanf("%d", &i);
 
-    printf("Preço (%.2f): ", 0);
-    scanf("%d", &novoVenda.preco);
+    while (!existeProduto(i)) {
+        printf("Produto não encontrado. Insira um código válido:\n");
+        printf("Código do produto: ");
+        scanf("%d", &i);
+    }
+
+    Produto produto = buscarProduto(i);
+    printf("Produto: %s\n", produto.descricao);
+    int c = confirm("Produto correto?");
+    while (!c) {
+        printf("Código do produto: ");
+        scanf("%d", &i);
+
+        while (!existeProduto(i)) {
+            printf("Produto não encontrado. Insira um código válido:\n");
+            printf("Código do produto: ");
+            scanf("%d", &i);
+        }
+        produto = buscarProduto(i);
+        printf("Produto: %s\n", produto.descricao);
+        c = confirm("Funcionário correto?");
+        i = 0;
+    }
+
+    novoVenda.produto_codigo = produto.codigo;
+
+    printf("Preço (%.2f): ", produto.preco);
+    scanf("%f", &novoVenda.preco);
+
+    printf("Quantidade: ");
+    scanf("%d", &novoVenda.quantidade);
+    printf("Valor total: %.2f\n", novoVenda.quantidade * novoVenda.preco);
 
     printf("Código do funcionario: ");
-    scanf("%d", &novoVenda.funcionario_codigo);
+    scanf("%d", &i);
+
+    while (!existeFuncionario(i)) {
+        printf("Funcionário não encontrado. Insira um código válido:\n");
+        printf("Código do funcionario: ");
+        scanf("%d", &i);
+    }
+
+    Funcionario funcionario = buscarFuncionario(i);
+    printf("Funcionário: %s\n", funcionario.nome);
+    c = confirm("Funcionário correto?");
+    while (!c) {
+        printf("Código do funcionario: ");
+        scanf("%d", &i);
+
+        while (!existeFuncionario(i)) {
+            printf("Funcionário não encontrado. Insira um código válido:\n");
+            printf("Código do funcionario: ");
+            scanf("%d", &i);
+        }
+        funcionario = buscarFuncionario(i);
+        printf("Funcionário: %s\n", funcionario.nome);
+        c = confirm("Funcionário correto?");
+        i = 0;
+    }
+
+    novoVenda.funcionario_codigo = funcionario.codigo;
 
     return novoVenda;
 }
